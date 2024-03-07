16a27367efaccc0ecec31a2a94ed337a080a9156
r12f <r12f.code@gmail.com>
2024-03-07 00:01:44 +0000

    Event.

--- experimental/saiswitchextensions.h
+++ experimental/saiswitchextensions.h
@@ -29,6 +29,55 @@
 #include <saitypes.h>
 
 /**
+ * @brief HA scope event type
+ */
+typedef enum _sai_ha_scope_event_t
+{
+    /** Moved to a new HA state. */
+    SAI_HA_SCOPE_STATE_CHANGED,
+
+} sai_ha_scope_event_t;
+
+/**
+ * @brief Notification data format received from SAI HA scope callback
+ *
+ * @count attr[attr_count]
+ */
+typedef struct _sai_ha_scope_event_data_t
+{
+    /** Event type */
+    sai_ha_scope_event_t event_type;
+
+    /** HA scope id */
+    sai_object_id_t ha_scope_id;
+
+    /** Attributes count */
+    uint32_t attr_count;
+
+    /**
+     * @brief Attributes
+     *
+     * @objects SAI_OBJECT_TYPE_HA_SCOPE
+     */
+    sai_attribute_t *attr;
+
+} sai_ha_scope_event_data_t;
+
+/**
+ * @brief HA scope event notification
+ *
+ * Passed as a parameter into sai_initialize_switch()
+ *
+ * @count data[count]
+ *
+ * @param[in] count Number of notifications
+ * @param[in] data Array of HA scope events
+ */
+typedef void (*sai_ha_scope_event_notification_fn)(
+        _In_ uint32_t count,
+        _In_ const sai_ha_scope_event_data_t *ha_scope_event_data);
+
+/**
  * @brief SAI switch attribute extensions.
  *
  * @flags free
@@ -36,6 +85,17 @@
 typedef enum _sai_switch_attr_extensions_t
 {
     SAI_SWITCH_ATTR_EXTENSIONS_RANGE_START = SAI_SWITCH_ATTR_END,
+
+    /**
+     * @brief HA scope event notification callback function passed to the adapter.
+     *
+     * Use sai_ha_scope_event_notification_fn as notification function.
+     *
+     * @type sai_pointer_t sai_ha_scope_event_notification_fn
+     * @flags CREATE_AND_SET
+     * @default NULL
+     */
+    SAI_SWITCH_ATTR_HA_SCOPE_EVENT_NOTIFY = SAI_SWITCH_ATTR_EXTENSIONS_RANGE_START,
 
     SAI_SWITCH_ATTR_EXTENSIONS_RANGE_END
 
